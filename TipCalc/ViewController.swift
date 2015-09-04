4eea4c1dee67a3b34be5e23b4e6c90f6723fb50a
Sean Narvasa <sean@snarvasa.com>
2015-09-03 21:33:06 -0700

    second commit

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var TipSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBAction func newTipPercentageSelected(sender: AnyObject) {
+        calculateTip()  //calls the calculateTip method
+        }
+    
+    @IBAction func billAmountChanged(sender: AnyObject) {
+        calculateTip() //calls the calculateTip method
+        }
+    
+    func calculateTip(){
+        var index = TipSegmentedControl.selectedSegmentIndex
+        var billAmount = (billTextField.text as NSString).doubleValue
+        var tipPercentage = 0.0
+        if index == 0 {
+            tipPercentage = 0.18
+        }
+        if index == 1 {
+            tipPercentage = 0.20
+        }
+        if index == 2 {
+            tipPercentage = 0.22
+        }
+        var tipAmount = billAmount * tipPercentage     //calcs the tipAmount
+        var totalAmount = billAmount + tipAmount       //calcs the totalAmount
+        
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        // converts the double into a string and gets two decimal places
+        totalLabel.text = String(format: "$%.2f", totalAmount)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
